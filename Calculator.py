ea6608a8b1e467e9d7497b213453d4c1fa74b2e3
KonstantinAbdusalamov <104074856+KonstantinAbdusalamov@users.noreply.github.com>
2022-05-06 11:55:23 +0300

    Update Calculator.py
    Изменения: калькулятор изменен на функцию.
    Добавлен цикл ,при ввводе "стоп" программа перестает работать и выходит из цикла. 
    Введен счетчик количества операций. 
    Добавлено исключение ошибок( деление на нуль)

--- Calculator.py
+++ Calculator.py
@@ -1,23 +1,32 @@
-
-text_first = input('Введите первое число: ')
-number_first = int(text_first)
-operation = input('Введите операцию: ')
-text_second = input('Введите второе число: ')
-number_second = int(text_second)
-if operation == '*':
-    result = number_first * number_second
-    print(result)
-elif operation == '+':
-    result = number_first + number_second
-    print(result)
-elif operation == '-':
-    result = number_first - number_second
-    print(result)
-elif operation == '/':
-    if number_second == 0:
-        print('Ошибка. Деление на нуль')
-    else:
-        result = number_first / number_second
+def calculator(first_number, operation, second_number):
+    if operation == '*':
+        result = first_number * second_number
         print(result)
+    elif operation == '+':
+        result = first_number + second_number
+        print(result)
+    elif operation == '-':
+        result = first_number - second_number
+        print(result)
+    elif operation == '/':
+        try:
+            result = first_number / second_number
+        except ZeroDivisionError:
+            print('Ошибка. Деление на нуль')
+        else:
+            print(result)
 
-# print("Ответ:", test_number)
+first_number = int(input('Введите первое число: '))
+operation = input('Введите операцию: ')
+second_number= int(input('Введите второе число: '))
+count = 1
+while operation != 'stop':
+    calculator(first_number, operation, second_number)
+    operation = input('Введите знак операции')
+    if operation != 'stop':
+        first_number = int(input('Введите первое число: '))
+        second_number = int(input('Введите второе число: '))
+    count =+ 1
+    print(f'Было выполнено - {count}')
+
+
